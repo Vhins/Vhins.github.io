652c49c2ab126c5b0e1c611af2b2ae4ef1275985
Vhins_ <vincenzomanzi06@gmail.com>
2025-06-24 21:00:44 +0200

    Memory, img links

--- memory/script.js
+++ memory/script.js
@@ -1,34 +1,3 @@
-const DECKS = {
-    "D1": [
-        {id: 1, link: "https://trucksrl.sirv.com/pub/media/catalog/product/i/m/image_5535_0.jpg?q=80&scale.option=fill&w=400&h=0"},
-        {id: 2, link: "https://trucksrl.sirv.com/pub/media/catalog/product/i/m/image_5536_0.jpg?q=80"},
-        {id: 3, link: "https://trucksrl.sirv.com/pub/media/catalog/product/i/m/image_5537_0.jpg?q=80"},
-        {id: 4, link: "https://trucksrl.sirv.com/pub/media/catalog/product/i/m/image_5538_0.jpg?q=80"},
-        {id: 5, link: "https://trucksrl.sirv.com/pub/media/catalog/product/i/m/image_5539_0.jpg?q=80"},
-        {id: 6, link: "https://trucksrl.sirv.com/pub/media/catalog/product/i/m/image_5540_0.jpg?q=80"},
-        {id: 7, link: "https://trucksrl.sirv.com/pub/media/catalog/product/i/m/image_5541_0.jpg?q=80"},
-        {id: 8, link: "https://trucksrl.sirv.com/pub/media/catalog/product/i/m/image_5542_0.jpg?q=80"},
-        {id: 9, link: "https://trucksrl.sirv.com/pub/media/catalog/product/i/m/image_5543_0.jpg?q=80"},
-        {id: 10, link: "https://image.spreadshirtmedia.net/image-server/v1/products/T1459A839PA4459PT28D157162510W8333H10000/views/1,width=550,height=550,appearanceId=839,backgroundColor=F2F2F2/numero-10-numero-dieci-calcio-playmaker-team-adesivo.jpg"},
-        {id: 11, link: "https://st2.depositphotos.com/1011969/6671/i/950/depositphotos_66714411-stock-photo-number-11.jpg"},
-        {id: 12, link: "https://image.spreadshirtmedia.net/image-server/v1/mp/products/T1459A839PA4459PT28D196924273W9731H10000/views/1,width=800,height=800,appearanceId=839,backgroundColor=F2F2F2/simbolo-del-numero-12-adesivo.jpg"},
-    ],
-
-    "D2": [
-        {id: 1, link: "https://www.laprovinciacr.it/resizer/480/-1/true/2023_10_08/Cattura-1696759177118.JPG--tutti_alla_ricerca_disperata_del_piccolo_cagnolino_athos.JPG?1696759177133"},
-        {id: 2, link: "https://www.ciamanimali.com/stupload/stblog/1/55/75/5575large.jpg"},
-        {id: 3, link: "https://www.villaggionatura.com/shop/modules/ph_simpleblog/covers/28.jpg"},
-        {id: 4, link: "https://safarizimbabwe.it/wp-content/uploads/2022/05/zebra-safari-zimbabwe-770x293.jpg"},
-        {id: 5, link: "https://www.my-personaltrainer.it/2023/09/19/quanto-vive-un-coniglio_900x760.jpeg"},
-        {id: 6, link: "https://citynews-romatoday.stgy.ovh/~media/horizontal-mid/42908841148321/tigre-di-sumatra.jpg"},
-        {id: 7, link: "https://www.focus.it/images/2022/09/20/ramphocelus-bresilius_1020x680.jpg"},
-        {id: 8, link: "https://www.focus.it/images/2023/08/03/grande-scimmia-leonina-orig.jpg"},
-        {id: 9, link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMflVx6-Kga8CurCYkzrGCnHKEYMeKoBg2oUhR3LMKdA&s"},
-        {id: 10, link: "https://www.focusjunior.it/content/uploads/site_stored/imgs/0001/048/koalafamiglia.jpg"},
-        {id: 11, link: "https://media-cdn.tripadvisor.com/media/photo-s/10/08/0f/ca/papera-bellissima.jpg"},
-        {id: 12, link: "https://sostieni.wwf.it/wp-content/uploads/2023/09/Immagini-new-sostieni-5800-x-4182-px-26-scaled.jpg"},
-    ]
-}
 
 let choosen_deck_type
 
@@ -161,9 +130,6 @@ async function flipCard(id) {
     can_flip = true
 
 }
-
-// carta.setAttribute("flipped", false)
-// carta.className = "non_girata"
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
